1e4fdddbf2c4a9df3ac168bcb20345f9e0b03e98
Tim <57293715+Gatekey@users.noreply.github.com>
2021-04-30 22:59:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, url_for
+import requests, json
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
