c7f243e021c6e19fa141c1aac9b75987a35500cc
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-05-01 19:12:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,20 @@ from flask import Flask, render_template, redirect, url_for
 import requests, json
 app = Flask(__name__)
 
-mins = 25
+mins = "25"
 sec = "00"
-sec = 00
 
 @app.route("/start")
 def start():
-    mins = 25
-    sec = 0
+    mins = "24"
+    sec = "59"
     print("hi")
     return render_template('index.html', minute = mins, seconds = sec)
+    
 
 @app.route("/")
 def main():
-    return render_template('index.html', minute = mins, seconds = sec)
-    mins = 25
-    sec = 0
-    return render_template('index.html', minute = 0, seconds = 0)
+    return render_template('index.html', minute = "25", seconds = "00")
 
 @app.route("/page2")
 def page2():
