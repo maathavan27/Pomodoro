c6600701580ff9f5b07dba8950c7781423c2dc72
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-05-01 18:57:14 -0400

    made play/restart do something

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ app = Flask(__name__)
 
 mins = 25
 sec = "00"
+sec = 00
 
+@app.route("/start")
 def start():
+    mins = 25
+    sec = 0
     print("hi")
+    return render_template('index.html', minute = mins, seconds = sec)
 
 @app.route("/")
 def main():
     return render_template('index.html', minute = mins, seconds = sec)
+    mins = 25
+    sec = 0
+    return render_template('index.html', minute = 0, seconds = 0)
 
 @app.route("/page2")
 def page2():
